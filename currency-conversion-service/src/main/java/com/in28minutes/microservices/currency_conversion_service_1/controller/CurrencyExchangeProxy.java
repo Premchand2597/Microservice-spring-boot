b2945314fad912df596ce3077c531c8d578cb0fe
Premchand <premsgowda25@gmail.com>
2026-01-19 20:16:51 +0530

    resolved github clone missing files - 19/01/2026

---
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currency_conversion_service_1/controller/CurrencyExchangeProxy.java
@@ -0,0 +1,15 @@
+package com.in28minutes.microservices.currency_conversion_service_1.controller;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.in28minutes.microservices.currency_conversion_service_1.bean.CurrencyConversion;
+
+
+//@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange")
+public interface CurrencyExchangeProxy {
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveData(@PathVariable String from, @PathVariable String to);
+}
---
+++ currency-conversion-service/src/test/java/com/in28minutes/microservices/currency_conversion_service_1/CurrencyConversionService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.currency_conversion_service_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
